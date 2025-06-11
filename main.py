5b49893d6299741e84b0e35e8621da90c7af63de
Muhammad Suleman Faisal <mfaisal.bese23seecs@student.nust.edu.pk>
2025-06-11 17:09:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import sys
 from cli import run_cli
+from gui import run_gui
 
 
 if __name__ == "__main__":
-    run_cli()
+    # Check if the user wants to use GUI or CLI
+    if len(sys.argv) > 1 and sys.argv[1].lower() == "--cli":
+        run_cli()
+    else:
+        # Default to GUI if no arguments or if anything other than --cli is specified
+        run_gui()
